58337c6b3525c8e6b24f8a2ed707ebba62aafaa2
LucasnDomingues <lucasn.domingues@gmail.com>
2020-07-21 20:22:31 -0300

    Adicionando os recursos de padronizacao de codigo

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const cors = require('cors');
 
-const routes = require("./routes");
+const routes = require('./routes');
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-
 let port = process.env.PORT;
 if (!port) {
-    port = 3333;
+	port = 3333;
 }
 
 app.listen(port);
