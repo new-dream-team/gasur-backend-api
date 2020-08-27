f6a46b246bc5b5872645a2a13b697c787355247e
LucasnDomingues <lucasn.domingues@gmail.com>
2020-08-27 20:54:20 -0300

    Correcao de import e melhoria na sintaxe dos JSONs

--- src/controller/ImageController.js
+++ src/controller/ImageController.js
@@ -1,16 +1,16 @@
 //index , show , store , update , destroy
-const Image = require('../models/Image').default;
+const Image = require('../models/Image');
 
 module.exports = {
 
     async store(req , res) {
         const {urlImage , name} = req.body;
-        const image = await Image.create({name : name , urlImage : urlImage});
+        const image = await Image.create({name, urlImage});
         return res.json(image);
     },
 
     async showAll(req , res) {
-        const image = await Image.find({});
+				const image = await Image.find({});
         return res.json(image);
     },
 
@@ -21,11 +21,12 @@ module.exports = {
 
     async delete(req , res) {
         await Image.deleteOne({_id : req.query.id});
-        return res.json({mensagem : "deleted"});
-    },
+        return res.json({message : "deleted"});
+		},
+
     async update(req , res) {
         const {urlImage , name} = req.body;
-        await Image.updateOne({ _id: req.query.id }, { name: name , urlImage: urlImage })
+        await Image.updateOne({ _id: req.query.id }, {name , urlImage })
         return res.json({mensagem: "updated" })
     }
 }
