e120a950bd35b6856628b561d90e62bf75f1fbb0
LucasnDomingues <lucasn.domingues@gmail.com>
2020-10-17 17:58:46 -0300

    Ajustando o CRUD e o Image model de persistencia

--- src/models/Image.js
+++ src/models/Image.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const ImageSchema = new mongoose.Schema({
     name: String,
-    urlImage: String,
+		urlImage: String,
+		poi: [],
+		points: [],
+}, {
+	versionKey: false,
 });
 
 module.exports = mongoose.model('Image', ImageSchema)
